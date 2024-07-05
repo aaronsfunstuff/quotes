e131f00a04eccffcda88b15cb2af3518d26f9663
aaronsfunstuff <135081747+aaronsfunstuff@users.noreply.github.com>
2024-07-05 15:49:51 -0700

    Create quotes.js

---
+++ quotes.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const quotes = [
+        "The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
+        "The purpose of our lives is to be happy. - Dalai Lama",
+        "Life is what happens when you're busy making other plans. - John Lennon",
+        "Get busy living or get busy dying. - Stephen King",
+        "You have within you right now, everything you need to deal with whatever the world can throw at you. - Brian Tracy",
+        "Believe you can and you're halfway there. - Theodore Roosevelt",
+        "The only impossible journey is the one you never begin. - Tony Robbins",
+        "Act as if what you do makes a difference. It does. - William James",
+        "Success is not the key to happiness. Happiness is the key to success. - Albert Schweitzer",
+        "Never bend your head. Always hold it high. Look the world straight in the eye. - Helen Keller",
+        "What we think, we become. - Buddha",
+      
